7dc564c9a8c144e536ece94f366d2f10826e1662
yinn_x <yinnnxt@gmail.com>
2024-01-26 01:45:29 +0800

    feat: add local config file for cmd-wrapped

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
 mod cli;
+mod config;
 mod history;
 mod parser;
 mod stats;
 mod view;
 
 use cli::Cli;
+use config::Config;
 use history::{History, HistoryProvider};
 use parser::CommandParser;
 use stats::Statistic;
 
 fn main() {
+    let config = Config::read();
     let args = Cli::parse_or_default();
     let provider = HistoryProvider::from(&args.shell);
     let history = History::from(&provider).expect("failed to read history file");
